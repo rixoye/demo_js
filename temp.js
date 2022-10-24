bb5e2bdb7756536595fd21a242993f758b7d9137
rixojojo <rixojojo@local.com>
2022-10-24 17:00:00 +0800

    create temp..js

---
+++ temp.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Hello world
+// @namespace    rixo.com/hello
+// @version      0.1
+// @description  Hello World
+// @author       rixo
+// @match        *://*/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    console.log("Hello World!");
+})();
