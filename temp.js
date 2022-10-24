aab53aa3c977e9bfca41b3f03d1fbca0d9f206fd
rixojojo <rixojojo@local.com>
2022-10-24 17:07:04 +0800

    add license

--- temp.js
+++ temp.js
@@ -7,6 +7,7 @@
 // @match        *://*/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
+// @license      MIT
 // ==/UserScript==
 
 (function() {
