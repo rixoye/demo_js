eea49ccbe75d55381a8f2606a29f1cdcc3828210
rixojojo <rixojojo@local.com>
2022-10-25 15:17:04 +0800

    添加延时输出功能

--- crt_sh.js
+++ crt_sh.js
@@ -11,29 +11,34 @@
 (() => {
     "use strict";
     let domain_set = new Set();
-    const elements = document.querySelectorAll("table>tbody>tr>td>table tr td:nth-last-of-type(2)")
-    if (elements.length > 0) {
-        for (let i = 0; i < elements.length; i++) {
-            const element = elements[i];
-            if (element.childElementCount === 0) {
-                //只有一个子域名
-                const raw = element.innerText?.trim();
-                domain_set.add(raw);
-            }else{
-                //有多个子域名
-                const nodes = element.childNodes;
-                for (let j = 0; j < nodes.length; j++) {
-                    const node = nodes[j];
-                    if (node.nodeType === Node.TEXT_NODE) {
-                      const raw = node.textContent?.trim();
-                      domain_set.add(raw);
+    setTimeout(() => {
+        getDomain();
+    }, 2000);
+    function getDomain(){
+        const elements = document.querySelectorAll("table>tbody>tr>td>table tr td:nth-last-of-type(2)")
+        if (elements.length > 0) {
+            for (let i = 0; i < elements.length; i++) {
+                const element = elements[i];
+                if (element.childElementCount === 0) {
+                    //只有一个子域名
+                    const raw = element.innerText?.trim();
+                    domain_set.add(raw);
+                }else{
+                    //有多个子域名
+                    const nodes = element.childNodes;
+                    for (let j = 0; j < nodes.length; j++) {
+                        const node = nodes[j];
+                        if (node.nodeType === Node.TEXT_NODE) {
+                        const raw = node.textContent?.trim();
+                        domain_set.add(raw);
+                        }
                     }
-                  }
+                }
             }
         }
-    }
 
-    for(let d of domain_set){
-        console.log(d);
+        for(let d of domain_set){
+            console.log(d);
+        }
     }
 })();
